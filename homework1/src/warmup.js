16115116012ce104b2800d4e13aba1d523db2333
obrienthomas13 <obrienthomas13@gmail.com>
2017-09-07 13:51:01 -0700

    Adding skeleton

---
+++ homework1/src/warmup.js
@@ -0,0 +1,39 @@
+function change () {
+  return false;
+}
+
+function stripQuotes () {
+  return false;
+}
+
+function scramble () {
+  return false;
+}
+
+function powers () {
+  return false;
+}
+
+function powersGenerator (base, exp) {
+  return false;
+}
+
+function say () {
+  return false;
+}
+
+function interleave () {
+  return false;
+}
+
+function cylinder () {
+  return false;
+}
+
+function makeCryptoFunctions () {
+  return false;
+}
+
+function randomName () {
+  return false;
+}
