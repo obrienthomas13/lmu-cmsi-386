f68d61f458f068f5cb956cfb132e10cb097bcea7
obrienthomas13 <obrienthomas13@gmail.com>
2017-09-07 17:32:32 -0700

    Adding scramble solution

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -12,8 +12,15 @@ exports.stripQuotes = function stripQuotes(str) {
   return str.replace(/['"]+/g, '');
 };
 
-exports.scramble = function scramble() {
-  return false;
+exports.scramble = function scramble(str) {
+  const result = [];
+  const strTemp = str.split('');
+  while (strTemp.length > 0) {
+    const rand = Math.floor(Math.random() * (strTemp.length));
+    result.push(strTemp[rand]);
+    strTemp.splice(rand, 1);
+  }
+  return result.join('');
 };
 
 exports.powers = function powers() {
