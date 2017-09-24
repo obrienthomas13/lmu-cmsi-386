0639ba68a8635ad976e04b3ed2bc68cd961cdce4
obrienthomas13 <obrienthomas13@gmail.com>
2017-09-24 16:13:55 -0700

    Setting up env, still needs linter

---
+++ homework2/warmup.py
@@ -0,0 +1,33 @@
+import requests
+# import pycrypto
+#        ^ Needs to be something like 'from Crypto.xxxx import xxxx'
+
+def change():
+    return False
+
+def strip_quotes():
+    return False
+
+def scramble():
+    return False
+
+def say():
+    return False
+
+def triples():
+    return False
+
+def powers():
+    return False
+
+def interleave():
+    return False
+
+def Cylinder():
+    return False
+
+def make_crypto_functions():
+    return False
+
+def random_name():
+    return False
