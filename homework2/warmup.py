2723f9a14d28d121203b19d027c6dbb8d0e70615
obrienthomas13 <obrienthomas13@gmail.com>
2017-09-24 18:05:31 -0700

    random_name() 'works', won't throw value error

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -2,32 +2,45 @@ import requests
 # import pycrypto
 #        ^ Needs to be something like 'from Crypto.xxxx import xxxx'
 
+
 def change():
     return False
+
 
 def strip_quotes():
     return False
 
+
 def scramble():
     return False
+
 
 def say():
     return False
 
+
 def triples():
     return False
+
 
 def powers():
     return False
 
+
 def interleave():
     return False
+
 
 def Cylinder():
     return False
 
+
 def make_crypto_functions():
     return False
 
-def random_name():
-    return False
+
+def random_name(**data):
+    payload = {'gender': data['gender'], 'region': data['region'], 'amount': 1}
+    person = requests.get('http://uinames.com/api/', params=payload).json()
+    return '{}, {}'.format(person['surname'], person['name'])
+    # return '{}, {}'.format(person.json()['surname'], person.json()['name']) if person.ok else raise ValueError('{}'.format(person.json()))
