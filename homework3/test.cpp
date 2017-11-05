6eed29e4226b79e3a27e9dd1ba27e14a0827eafa
DrowninFish <tedmist1@lion.lmu.edu>
2017-11-05 15:05:23 -0800

    test.cpp works for wordcount

--- homework3/test.cpp
+++ homework3/test.cpp
@@ -6,9 +6,19 @@
 
 using namespace std;
 
+int findIndex(vector< pair <string, int> > vec, string s){
+  for(int i = 0; i < vec.size(); i++) {
+    if(vec[i].first == s){
+      return i;
+    }
+  }
+  return -1;
+}
+
+
 int main() {
 
-    string s = "Hello ha8pPy   day m8e";
+    string s = "Hello ha8pPy h/;^e78llo  23a8g a    da3^a#$y m8e hello";
 
     vector< pair <string, int> > potato;
 
@@ -17,23 +27,24 @@ int main() {
     for(int i = 0; i < s.length(); i++) {
       sclone += (char)tolower(s.at(i));
     }
-
+    sclone += " ";
     int prev = 0;
     int current = 0;
     while(current < sclone.length()) {
-      if(sclone.at(current) == ' '&& prev != current){
-        potato.push_back(make_pair(sclone.substr(prev, current-prev), 1));
-        prev = current + 1;
-        current ++;
-      }
-      if(prev == current && sclone.at(current) == ' '){
-        prev++;
+      if(!isalpha(sclone.at(current))){
+        if(prev != current) {
+          if(findIndex(potato, sclone.substr(prev, current-prev)) == -1) {
+            potato.push_back(make_pair(sclone.substr(prev, current-prev), 1));
+          } else {
+            potato[findIndex(potato, sclone.substr(prev, current-prev))].second++;
+          }
+          prev = current + 1;
+        } else {
+          prev++;
+        }
       }
       current ++;
     }
-    potato.push_back(make_pair(sclone.substr(prev, current-prev), 1));
-
-
     for(int i = 0; i < potato.size(); i++) {
       cout << potato[i].first << " : " << potato[i].second <<  '\n';
     }
