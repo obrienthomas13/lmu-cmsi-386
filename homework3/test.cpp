222eefeaf63b2e7d3a3ce4eb08775feb3160df46
DrowninFish <tedmist1@lion.lmu.edu>
2017-11-12 13:14:00 -0800

    Queue in test file

--- homework3/test.cpp
+++ homework3/test.cpp
@@ -1,58 +1,86 @@
+#include <cassert>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <utility>
-#include <stdexcept>
 
 using namespace std;
 
-// Returns the index of a given string in a vector, or -1 if string doesnt exist yet
-int findIndex(vector< pair <string, int> > vec, string s){
-  for(int i = 0; i < vec.size(); i++) {
-    if(vec[i].first == s){
-      return i;
+template <typename T>
+class Queue {
+
+  struct Node {
+    T data;
+    Node* next;
+  };
+
+  int size = 0;
+  Node* tail = nullptr;
+  Node* head = nullptr;
+
+  Node* copy(Node* n) {
+    delete;
+    return new Node {n->data, n->next ? copy(n->next) : nullptr};
+  }
+
+
+public:
+
+  Queue(const Queue& a) = delete;
+  void operator=(const Queue& a) = delete;
+  ~Queue() {
+    while (head != nullptr) {
+      pop();
     }
   }
-  return -1;
-}
 
+  Queue() = default;
+
+  int get_size() {
+    return size;
+  }
+
+  T get_tail() {
+    return tail->data;
+  }
+
+  void enqueue(T x) {
+    Node newTail = new Node {x, tail};
+    tail = &newTail;
+    size++;
+  }
+
+  T dequeue() {
+    Node* nodeToDelete = head;
+    T valueToReturn = head->data;
+    delete nodeToDelete;
+
+    Node findNewHead = *tail;
+    while(findNewHead.next != nullptr) {
+      findNewHead = *(findNewHead.next);
+    }
+    head = &findNewHead;
+    size--;
+    return valueToReturn;
+  }
+
+};
 
 int main() {
+  Queue<int> s;
+  assert(s.get_size() == 0);
+  s.enqueue(100);
+  assert(s.get_tail() == 100);
+  assert(s.get_size() == 1);
+  s.enqueue(200);
+  assert(s.get_tail() == 200);
+  assert(s.get_size() == 2);
+  assert(s.dequeue() == 200);
+  assert(s.get_size() == 1);
+  assert(s.dequeue() == 100);
+  assert(s.get_size() == 0);
+  s.enqueue(300);
+  s.enqueue(400);
 
-    string s = "Hello ha8pPy h/;^e78llo  23a8g a    da3^a#$y m8e hello";
+  cout << "All tests passed\n";
 
-    vector< pair <string, int> > vec;
+  // Queue<int> t = s;   // COPY CONSTRUCTOR
 
-    // Changes the string into lower case
-    string sclone = "";
-    for(int i = 0; i < s.length(); i++) {
-      sclone += (char)tolower(s.at(i));
-    }
-    // Adds a empty char at the end so the parse gets the last character
-    sclone += " ";
-    int prev = 0;
-    int current = 0;
-    // Loops through the length of the string
-    while(current < sclone.length()) {
-      if(!isalpha(sclone.at(current))){
-        // This is the case that we have a desired string next
-        if(prev != current) {
-          if(findIndex(vec, sclone.substr(prev, current-prev)) == -1) {
-            vec.push_back(make_pair(sclone.substr(prev, current-prev), 1));
-          } else {
-            vec[findIndex(vec, sclone.substr(prev, current-prev))].second++;
-          }
-          prev = current + 1;
-        // This is the case that we have 2+ undesired characters in a row
-        } else {
-          prev++;
-        }
-      }
-      current ++;
-    }
-
-    // Prints out the  whole vector
-    for(int i = 0; i < vec.size(); i++) {
-      cout << vec[i].first << " : " << vec[i].second <<  '\n';
-    }
 }
