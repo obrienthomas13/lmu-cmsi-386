1dd536f698cb2342a62dbfe2f1acc94906a6e521
obrienthomas13 <obrienthomas13@gmail.com>
2017-11-14 21:24:40 -0800

    Adding test to queue_test.cpp

--- homework3/queue_test.cpp
+++ homework3/queue_test.cpp
@@ -6,14 +6,11 @@
 using namespace std;
 
 void test_empty_queue_has_zero_size() {
-  cout << "Start Test 1\n";
   Queue<string> q;
   assert(q.get_size() == 0);
-  cout << "Test 1\n";
 }
 
 void test_some_insertions_and_deletions() {
-  cout << "Start Test 2\n";
   Queue<string> q;
   q.enqueue("one");
   assert(q.get_size() == 1);
@@ -26,18 +23,15 @@ void test_some_insertions_and_deletions() {
   assert(q.dequeue() == "three");
   q.enqueue("four");
   assert(q.get_size() == 1);
-  cout << "Test 2\n";
 }
 
 void test_no_copies() {
-  cout << "Start Test 3\n";
   Queue<int> p;
   Queue<int> q;
   // No way to test this at run time, but uncomment the following lines
   // and look for compile time errors.
   // p = q;
   // Queue<int> r(p);
-  cout << "Test 3\n";
 }
 
 Queue<int> one_two_three() {
@@ -47,7 +41,6 @@ Queue<int> one_two_three() {
 }
 
 void test_moves() {
-  cout << "Start Test 4\n";
   // Assignment of a temporary is a move
   Queue<int> p = Queue<int>();
 
@@ -60,86 +53,50 @@ void test_moves() {
   q = one_two_three();
   assert(q.get_size() == 3);
   assert(q.dequeue() == 1);
-  cout << "Test 4\n";
 }
 
 void test_dequeue_from_empty_queue_throws_underflow_error() {
-  cout << "Start Test5\n";
   Queue<bool> q;
   try {
     q.dequeue();
     assert(false);
   } catch (const underflow_error& ue) {
     assert(true);
-    cout << "Test5\n";
   } catch (...) {
     // Caught the wrong exception
     assert(false);
   }
 }
 
-int main() {
-  Queue<int> s;
-  assert(s.get_size() == 0);
-  s.enqueue(100);
-  assert(s.get_tail() == 100);
-  assert(s.get_head() == 100);
-  assert(s.get_size() == 1);
-  s.enqueue(200);
-  assert(s.get_tail() == 200);
-  assert(s.get_head() == 100);
-  assert(s.get_size() == 2);
-  s.enqueue(300);
-  assert(s.get_tail() == 300);
-  assert(s.get_head() == 100);
-  assert(s.get_size() == 3);
-  s.enqueue(400);
-  assert(s.get_tail() == 400);
-  assert(s.get_head() == 100);
-  assert(s.get_size() == 4);
-  s.enqueue(500);
-  assert(s.get_tail() == 500);
-  assert(s.get_head() == 100);
-  assert(s.get_size() == 5);
-  s.dequeue();
-  assert(s.get_tail() == 500);
-  assert(s.get_head() == 200);
-  assert(s.get_size() == 4);
-  s.dequeue();
-  assert(s.get_tail() == 500);
-  assert(s.get_head() == 300);
-  assert(s.get_size() == 3);
-  s.dequeue();
-  assert(s.get_tail() == 500);
-  assert(s.get_head() == 400);
-  assert(s.get_size() == 2);
-  s.dequeue();
-  assert(s.get_tail() == 500);
-  assert(s.get_head() == 500);
-  assert(s.get_size() == 1);
-  s.dequeue();
-  cout << "before enqueue\n";
-  s.enqueue(2);
-  cout << "after enqueue\n";
-  cout << "get_tail()\n";
-  assert(s.get_tail() == 2);
-  cout << "get_head()\n";
-  assert(s.get_size() == 1);
-  assert(s.get_head() == 2);
-  s.enqueue(3);
-  assert(s.get_tail() == 3);
-  assert(s.get_head() == 2);
-  assert(s.get_size() == 2);
+void test_enqueue_then_dequeue_then_enqueue() {
+  Queue<int> q;
+  assert(q.get_size() == 0);
+  q.enqueue(100);
+  assert(q.get_tail() == 100);
+  assert(q.get_head() == 100);
+  assert(q.get_size() == 1);
+  q.enqueue(200);
+  assert(q.get_tail() == 200);
+  assert(q.get_head() == 100);
+  assert(q.get_size() == 2);
+  q.dequeue();
+  assert(q.get_tail() == 200);
+  assert(q.get_head() == 200);
+  assert(q.get_size() == 1);
+  q.dequeue();
+  assert(q.get_size() == 0);
+  q.enqueue(300);
+  assert(q.get_tail() == 300);
+  assert(q.get_head() == 300);
+  assert(q.get_size() == 1);
+}
 
-  // cout << "before1\n";
-  // test_empty_queue_has_zero_size();
-  // cout << "before2\n";
-  // test_some_insertions_and_deletions();
-  // cout << "before3\n";
-  // test_dequeue_from_empty_queue_throws_underflow_error();
-  // cout << "before4\n";
-  // test_no_copies();
-  // cout << "before5\n";
-  // test_moves();
+int main() {
+  test_empty_queue_has_zero_size();
+  test_some_insertions_and_deletions();
+  test_dequeue_from_empty_queue_throws_underflow_error();
+  test_no_copies();
+  test_moves();
+  test_enqueue_then_dequeue_then_enqueue();
   cout << "All tests passed\n";
 }
