ffedcf1e900f45a70ac790ab50501d47d1d61286
obrienthomas13 <obrienthomas13@gmail.com>
2017-11-29 16:56:00 -0800

    Change works, worth improving

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -1,8 +1,18 @@
 module Warmup exposing (..)
 import Regex exposing (..)
 
-change: Int -> (Int, Int, Int, Int)
-change s = (0, 0, 0, 0)
+change: Int -> Result String (Int, Int, Int, Int)
+change cents =
+  if (<) cents 0 then
+    Err "amount cannot be negative"
+  else
+    let
+      quarters = (//) cents 25
+      quartersValue = (-) cents ((*) quarters 25)
+      dimes = (//) (quartersValue) 10
+      nickels = (//) ((-) quartersValue ((*) dimes 10)) 5
+    in
+      Ok (quarters, dimes, nickels, cents % 5)
 
 stripQuotes: String -> String
 stripQuotes =
