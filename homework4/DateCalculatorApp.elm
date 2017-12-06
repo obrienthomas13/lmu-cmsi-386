37febbe9e818f7a69a4c8f0626422a8e3394a89f
obrienthomas13 <obrienthomas13@gmail.com>
2017-12-06 00:24:02 -0800

    DateCalcApp works, could be DRYer

--- homework4/DateCalculatorApp.elm
+++ homework4/DateCalculatorApp.elm
@@ -6,10 +6,11 @@ import Warmup exposing (daysBetween)
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
+-- MODEL
+
 type alias Model =
-  {
-    from : String
-    , to : String
+  { from : String
+  , to : String
   }
 
 model : Model
@@ -20,15 +21,15 @@ model =
 
 -- UPDATE
 
-type Msg =
-  ChangeFrom String
-  | ChangeTo String
+type Msg = ChangeFrom String | ChangeTo String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    ChangeFrom fromDate -> { model | from = fromDate }
-    ChangeTo toDate -> { model | to = toDate }
+    ChangeFrom fromDate ->
+      { model | from = fromDate }
+    ChangeTo toDate ->
+      { model | to = toDate }
 
 dateCalcMessage : String -> String -> String
 dateCalcMessage fromDate toDate =
@@ -36,71 +37,76 @@ dateCalcMessage fromDate toDate =
       Ok days -> toString days
       _ -> ""
 
+-- VIEW
+
 view : Model -> Html Msg
 view model =
   bodyStylized
-  [
-    h1Stylized "Date Calculator"
-    , p [] [
-      text "From "
+    [ h1Stylized "Date Calculator"
+    , p []
+      [ text "From "
       , inputStylized1 "from"
-    ]
-    , p [] [
-      text "To "
+      ]
+    , p []
+      [ text "To "
       , inputStylized2 "to"
-    ]
-    , p [] [
-      text "is "
-      , span [id "output"] [text <| dateCalcMessage model.from model.to]
+      ]
+    , p []
+      [ text "is "
+      , spanStylized "output" (dateCalcMessage model.from model.to)
       , text " days."
-    ]
-
+      ]
   ]
+
+spanStylized: String -> String -> Html msg
+spanStylized idName msg =
+  span
+    [ id idName
+    , style [ ("font-size", "28px") ]
+    ] [ text msg ]
 
 bodyStylized: List (Html msg) -> Html msg
 bodyStylized htmlMsg =
   body
-  [ style
-    [ ("text-align", "center")
+    [ style
+      [ ("text-align", "center")
       , ("font", "16px Arial")
       , ("background-color", "linen")
       , ("margin", "0")
-    ]
-  ] htmlMsg
+      ]
+    ] htmlMsg
 
 h1Stylized : String -> Html msg
 h1Stylized msg =
   h1
-  [ style
-    [ ("font", "bold 40px Avenir")
+    [ style
+      [ ("font", "bold 40px Avenir")
       , ("margin-top", "0")
       , ("padding", "5px")
       , ("background-color", "cyan")
-    ]
-  ] [ text msg ]
+      ]
+    ] [ text msg ]
 
 inputStylized1 : String -> Html Msg
 inputStylized1 idName =
   input
-  [ style
-    [ ("border", "2px solid grey")
+    [ style
+      [ ("border", "2px solid grey")
       , ("margin-left", "8px")
-    ]
-    , id idName
-    , type_ "date"
-    , onInput ChangeFrom
-    , value model.from
-  ] []
+      ]
+      , id idName
+      , type_ "date"
+      , onInput ChangeFrom
+    ] []
 
 inputStylized2 : String -> Html Msg
 inputStylized2 idName =
   input
-  [ style
-    [ ("border", "2px solid grey")
+    [ style
+      [ ("border", "2px solid grey")
       , ("margin-left", "8px")
-    ]
-    , id idName
-    , type_ "date"
-    , onInput ChangeTo
-    , value model.to
-  ] []
+      ]
+      , id idName
+      , type_ "date"
+      , onInput ChangeTo
+    ] []
